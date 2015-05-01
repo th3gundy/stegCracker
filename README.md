b5eb505e769718a9c49d4b4130e5159ebd5ea073
Th3GundY <yunus__yildirim@hotmail.com>
2015-05-01 18:32:29 +0000

    First pusht

---
+++ README.md
@@ -0,0 +1,8 @@
+# stegCracker
+
+CTF'lerde Steganography kategorisindeki bazı sorular steghide aracıyla çözülmektedir, flag dosyası resimden elde edilmektedir.
+Bazı durumlarda bu işlem belirlenmiş bir parola ile gerçekleşir.
+
+Bu tool argüman olarak aldığı wordList'teki parolaları resim için dener.
+
+# 5000 kelimeyi ortalama 30-15 saniyede denemektedir. 
---
+++ steghideCracker.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+    
+def stegCracker(wlistlocation,stegFile):
+	wlist=open(wlistlocation,"r")
+    	print "Going to password crack "+stegFile+" with wordlist "+wlistlocation
+	tries=0
+    	start=time.time()
+	try:
+	    	for word in wlist:
+    			if(word[-1]=="\n"):
+    				word=word[:-1]
+	    		if word=="":
+    				word="NULL"
+	    		if word[0]=="#" or word[0]=="$" or word[0]=="&":
+    				print "Passing "+word
+    				word="NULL"
+	    		if word!="NULL":
+				print "Trying "+word
+    				a=os.popen('steghide extract -p '+word+' -sf '+stegFile+' 2>&1 | grep "wrote extracted"')
+				tries+=1
+    				for i in a.readlines():
+    					if i.find('wrote') != -1:
+    						print " [+] Found password : "+repr(word)
+    						print " [+] Passwords tried : "+str(tries)
+	    					print " [+] Time : %.2f seconds" %(time.time()-start)
+    						sys.exit(1)
+	except KeyboardInterrupt:
+		print "\n[-]Keyboard Interruption"
+if(len(sys.argv)==3):
+	stegCracker(sys.argv[1],sys.argv[2])
+else:
+	print sys.argv[0]+" wordList imageFile"
+	print "Example: "+sys.argv[0]+" pass.list test.jpg"
