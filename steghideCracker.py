72e1b0f41e7c5af2d9637373002f436ec1dd8ae3
Yunus YILDIRIM <th3gundy@users.noreply.github.com>
2016-10-25 09:58:15 +0300

    fix some bugs

--- steghideCracker.py
+++ steghideCracker.py
@@ -9,23 +9,16 @@ def stegCracker(wlistlocation,stegFile):
     	start=time.time()
 	try:
 	    	for word in wlist:
-    			if(word[-1]=="\n"):
-    				word=word[:-1]
-	    		if word=="":
-    				word="NULL"
-	    		if word[0]=="#" or word[0]=="$" or word[0]=="&":
-    				print "Passing "+word
-    				word="NULL"
-	    		if word!="NULL":
-				print "Trying "+word
-    				a=os.popen('steghide extract -p '+word+' -sf '+stegFile+' 2>&1 | grep "wrote extracted"')
-				tries+=1
-    				for i in a.readlines():
-    					if i.find('wrote') != -1:
-    						print " [+] Found password : "+repr(word)
-    						print " [+] Passwords tried : "+str(tries)
-	    					print " [+] Time : %.2f seconds" %(time.time()-start)
-    						sys.exit(1)
+			word = word.strip()
+			print "Trying "+word
+			a=os.popen("steghide extract -p '"+word+"' -sf "+stegFile+" 2>&1 | grep 'wrote extracted'")
+			tries+=1
+  			for i in a.readlines():
+    				if i.find('wrote') != -1:
+    					print " [+] Found password : "+repr(word)
+    					print " [+] Passwords tried : "+str(tries)
+	    				print " [+] Time : %.2f seconds" %(time.time()-start)
+    					sys.exit(1)
 	except KeyboardInterrupt:
 		print "\n[-]Keyboard Interruption"
 if(len(sys.argv)==3):
@@ -33,3 +26,4 @@ if(len(sys.argv)==3):
 else:
 	print sys.argv[0]+" wordList imageFile"
 	print "Example: "+sys.argv[0]+" pass.list test.jpg"
+	
